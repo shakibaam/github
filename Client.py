47f1df775b723be0c129fbbb5ff49da1e4761c8f
shakibaam <shakibaamirshahi@yahoo.com>
2021-05-17 22:24:28 +0330

    some bugs fixed

--- Client.py
+++ Client.py
@@ -1,14 +1,16 @@
 import os
 import socket
-PORT=7447
-MESSAGE_LEN_SIZE=1024
-ENCODING ='utf-8'
-NAME=""
+
+PORT = 7447
+MESSAGE_LEN_SIZE = 1024
+ENCODING = 'utf-8'
+NAME = ""
+
 
 def main():
     global NAME
-    NAME=input("we need to know who are you?")
-    create_dir(NAME,'C:\\Users\\Asus\\PycharmProjects\\CN_P2\\Clients')
+    NAME = input("we need to know who are you?")
+    create_dir(NAME, 'C:\\Users\\Asus\\PycharmProjects\\CN_P2\\Clients')
     addrss = socket.gethostbyname(socket.gethostname())
     Host_info = ("192.168.131.1", PORT)
 
@@ -16,126 +18,117 @@ def main():
     s.connect(Host_info)
     message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
     if message:
-     print(message)
+        print(message)
 
     while True:
 
-     Request=input()
-     send_msg(s,Request)
-     if Request == "disconnect":
-         message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-         if message:
-             print(message)
-         exit()
+        Request = input()
+        send_msg(s, Request)
+        if Request == "disconnect":
+            message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
+            if message:
+                print(message)
+            exit()
 
-     if Request=="sign up" :
-         message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-         if message:
-             print(message)
+        if Request == "sign up":
+            message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
+            if message:
+                print(message)
 
-     if Request == "sign in":
-         message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-         if message:
-             print(message)
+        if Request == "sign in":
+            message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
+            if message:
+                print(message)
 
-     if "lets Go" in Request:
-         message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-         if message:
-             print(message)
+        if "lets Go" in Request:
+            message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
+            if message:
+                print(message)
 
-     if "log in" in Request:
-         message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-         if message:
-             print(message)
+        if "log in" in Request:
+            message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
+            if message:
+                print(message)
 
-     if "create_Repo" in Request:
-         message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-         if message:
-             print(message)
+        if "create_Repo" in Request:
+            message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
+            if message:
+                print(message)
 
-     if "subDir" in Request:
-         message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-         if message:
-             print(message)
+        if "subDir" in Request:
+            message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
+            if message:
+                print(message)
 
-     if "want push" in Request:
-         message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-         if message:
-             print(message)
+        if "want push" in Request:
+            message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
+            if message:
+                print(message)
 
-         big_string = ""
-         file_name = input("Enter file name")
-         big_string += "Go to Push#"
-         big_string += str(file_name)
-         big_string += '%'
-         file_path_local = input("Enter file path local")
-         Repo = input("Enter which Repo")
-         big_string += Repo
+            big_string = ""
+            file_name = input("Enter file name")
+            big_string += "Go to Push#"
+            big_string += str(file_name)
+            big_string += '%'
+            file_path_local = input("Enter file path local")
+            Repo = input("Enter which Repo")
+            big_string += Repo
 
-         with open(f"{file_path_local}", "r") as f:
-             content = f.read()
+            with open(f"{file_path_local}", "r") as f:
+                content = f.read()
 
-         big_string += "&"
-         big_string += str(content)
-         print(big_string)
-         big_string+="$"
-         commit=input("Enter commit message")
-         big_string+=commit
-         send_msg(s, big_string)
-         message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-         if message:
-             print(message)
-     if Request == "ok":
-         message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-         if message:
-             print(message)
+            big_string += "&"
+            big_string += str(content)
+            print(big_string)
+            big_string += "$"
+            commit = input("Enter commit message")
+            big_string += commit
+            send_msg(s, big_string)
+            message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
+            if message:
+                print(message)
+        if Request == "ok":
+            message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
+            if message:
+                print(message)
 
-     if "want pull" in Request:
-         message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-         if message:
-             print(message)
-
-         pull_string=""
-         pull_string+="please pull#"
-         which_user=input("Enter which user you  want?")
-         which_repo=input("Enter which repo from {}".format(which_user))
-         pull_string+=str(which_user)
-         pull_string+="#"
-         pull_string+=str(which_repo)
-         send_msg(s, pull_string)
-         Repo_address = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
-
-         if Repo_address:
-             print(Repo_address)
-             create_dir(which_repo,os.path.join("C:\\Users\\Asus\\PycharmProjects\\CN_P2\\Clients",NAME))
-             pull_path=os.path.join("C:\\Users\\Asus\\PycharmProjects\\CN_P2\\Clients",NAME,which_repo)
+        if "want pull" in Request:
+            message = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
 
 
-             for root, subdirectories, files in os.walk(Repo_address):
-                 root1=root.replace(Repo_address,os.path.join("C:\\Users\\Asus\\PycharmProjects\\CN_P2\\Clients",NAME,which_repo))
-                 for subdirectory in subdirectories:
-                     print(os.path.join(root1, subdirectory))
-                     create_dir(subdirectory,root1)
-                 for file in files:
-                     print(os.path.join(root1, file))
+            pull_string = ""
+            pull_string += "please pull#"
+            which_user = input("Enter which user you  want?")
+            which_repo = input("Enter which repo from {}".format(which_user))
+            pull_string += str(which_user)
+            pull_string += "#"
+            pull_string += str(which_repo)
+            send_msg(s, pull_string)
+            Repo_address = (s.recv(MESSAGE_LEN_SIZE)).decode(ENCODING)
+            if  "nothing found" in Repo_address:
+                print(Repo_address)
 
-                     with open(f"{os.path.join(root, file)}", "r") as f:
-                         content = f.read()
+            else:
+                print(Repo_address)
+                create_dir(which_repo, os.path.join("C:\\Users\\Asus\\PycharmProjects\\CN_P2\\Clients", NAME))
+                pull_path = os.path.join("C:\\Users\\Asus\\PycharmProjects\\CN_P2\\Clients", NAME, which_repo)
 
-                     with open(os.path.join(root1, file), "w") as f:
-                        f.write(content)
-                 print("pull successfully =))")
+                for root, subdirectories, files in os.walk(Repo_address):
+                    root1 = root.replace(Repo_address,
+                                         os.path.join("C:\\Users\\Asus\\PycharmProjects\\CN_P2\\Clients", NAME,
+                                                      which_repo))
+                    for subdirectory in subdirectories:
+                        print(os.path.join(root1, subdirectory))
+                        create_dir(subdirectory, root1)
+                    for file in files:
+                        print(os.path.join(root1, file))
 
+                        with open(f"{os.path.join(root, file)}", "r") as f:
+                            content = f.read()
 
-
-
-
-
-
-
-
-
-         else:print("Nothing found...Try Again")
+                        with open(os.path.join(root1, file), "w") as f:
+                            f.write(content)
+                print("pull successfully =))")
 
 
 
@@ -144,30 +137,28 @@ def main():
 
 
 
-def send_msg(client,msg):
-
-     message = msg.encode(ENCODING)
-     msg_len= len(message)
-     msg_len =str(msg_len).encode(ENCODING)
-     msg_len +=b' '*(MESSAGE_LEN_SIZE-len(msg_len))
-     client.send(msg_len)
-     client.send(message)
 
 
+
+
+
+
+def send_msg(client, msg):
+    message = msg.encode(ENCODING)
+    msg_len = len(message)
+    msg_len = str(msg_len).encode(ENCODING)
+    msg_len += b' ' * (MESSAGE_LEN_SIZE - len(msg_len))
+    client.send(msg_len)
+    client.send(message)
 
 
 def create_dir(dir_name, paren_path):
     directory = dir_name
     path = os.path.join(paren_path, directory)
-    if  not os.path.exists(path):
-
+    if not os.path.exists(path):
         os.mkdir(path)
         print("Directory '% s' created" % directory)
 
 
-
-
-
-
-if __name__ =='__main__':
-  main()
+if __name__ == '__main__':
+    main()
